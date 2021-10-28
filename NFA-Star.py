3e1f261014077d6533adf720ad9445d10e84c1bd
Lamli <nguyenli23@up.edu>
2021-10-28 13:52:57 -0700

    created test json file

--- NFA-Star.py
+++ NFA-Star.py
@@ -1,5 +1,23 @@
 import sys
 import json
 
+
 def readFile(file):
-    
+
+  
+
+    return
+
+f = open('testing.json',)
+
+data = json.load(f)
+
+for i in data:
+    if i == "setOfStates":
+        statesSet = data[i]
+    elif i == "alphabet":
+        alpha = data[i]
+
+print(statesSet)
+print(statesSet[0])
+
