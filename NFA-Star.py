276b0838a5fddc9d313dfaa71f244a4cc7b51a6d
Lamli <nguyenli23@up.edu>
2021-10-28 14:12:15 -0700

    examples on how to access transitions

--- NFA-Star.py
+++ NFA-Star.py
@@ -4,8 +4,6 @@ import json
 
 def readFile(file):
 
-  
-
     return
 
 f = open('testing.json',)
@@ -17,7 +15,17 @@ for i in data:
         statesSet = data[i]
     elif i == "alphabet":
         alpha = data[i]
+    elif i == "transitions":
+        trans = data[i]
+    elif i == "startState":
+        start = data[i]
+    elif i == "acceptingState":
+        accept = data[i]
 
-print(statesSet)
-print(statesSet[0])
+#print(statesSet)
+#print(statesSet[0])
+
+#print(trans)
+print(trans['q0']['b'])
+
 
