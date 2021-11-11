a742aaec16e1a23d909de5d9f0963f72cd6f9490
Lamli <nguyenli23@up.edu>
2021-11-11 13:10:14 -0800

    Created new transitions and addded new accepting state functionality

--- NFA-Star.py
+++ NFA-Star.py
@@ -25,7 +25,40 @@ for i in data:
 #print(statesSet)
 #print(statesSet[0])
 
-#print(trans)
-print(trans['q0']['b'])
+#transition from q0 on b
+#print(trans['q0']['b'])
+
+#make new transition from accepting state to initial start state
+
+#create a temporary state and make it transition from
+#accepting state to start state for A*
+temp = trans[accept[0]]
+temp["E"] = start
+
+#Overwrite the accepting state with the temp state
+data['transitions'][accept[0]] = temp
+
+#make new start state q'
+statesSet.insert(0, 'q\'')
+
+#Make transition from q' to initial start state
+trans['q\''] = {} 
+newStart = trans['q\'']
+newStart["E"] = start
+data['transitions']['q\''] = newStart
+
+temp2 = {"q'":newStart}
+
+temp2.update(trans)
+
+
+#set new state to be q'
+start = 'q\''
+
+accept.insert(0, 'q\'')
+
+print(temp2)
+
+print(accept)
 
 
